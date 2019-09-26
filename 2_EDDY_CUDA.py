a12239df663c6087600316262b7cc6d3d604e3d7
Lucinda Sisk <lucinda.sisk@gmail.com>
2019-09-26 16:30:26 -0400

    fix mask template

--- 2_EDDY_CUDA.py
+++ 2_EDDY_CUDA.py
@@ -63,7 +63,7 @@ infosource = Node(IdentityInterface(fields=['subject_id']),
 infosource.iterables = [('subject_id', subject_list)]
 
 # SelectFiles
-template = dict(mask=join(home, 'data/2_Preprocessed/{subject_id}/{subject_id}_ses-shapesV1_T1w_resample_brain_mask.nii.gz'),
+template = dict(mask=join(home, 'data/2_Preprocessed/{subject_id}/{subject_id}_ses-shapesV1_T1w_flirt_brain_mask.nii.gz'),
                 dti=join(
                     home, 'data/2_Preprocessed/{subject_id}/denoised_gibbs_bias_corrected_reoriented_flirt.nii.gz'),
                 bval=join(
