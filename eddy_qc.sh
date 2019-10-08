62c0fb33623c22ad2225cee02efe0dc8b3e0f93c
Lucinda Sisk <lucinda.sisk@gmail.com>
2019-10-08 11:30:13 -0400

    add eddyqc script

---
+++ eddy_qc.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+#Usage: ./eddy_qc <name_of_subject_list.txt
+
+home='/gpfs/milgram/project/gee_dylan/candlab/'
+dwi='/gpfs/milgram/project/gee_dylan/candlab/analyses/shapes/dwi'
+subs=`cat $home'/scripts/shapes/mri/dwi/Diffusion/'$1`
+
+if [ -d $home'/analyses/shapes/dwi/data/4_Eddy_QC_Data' ] ; then
+  pass
+else
+  mkdir $home'/analyses/shapes/dwi/data/4_Eddy_QC_Data'
+
+for sub in $subs; do
+  fldr=$home'/analyses/shapes/dwi/data/4_Eddy_QC_Data'
+  mkdir $fldr'/sub'
+
+  eddy_quad $home'/data/3_EddyCorrected/'$sub'/eddy_corrected' \
+  -idx $dwi'/shapes_index.txt' \
+  -par $dwi'/shapes_acqparams.txt' \
+  -m $home'/data/3_EddyCorrected/'$sub'/b0_img_brain_mask.nii.gz' \
+  -b $home'/data/mri/bids_recon/shapes/'$sub'/ses-shapesV1/dwi/sub-A201_ses-shapesV1_dwi.bval' \
+  -o $fldr'/sub'
