681f8c0b8ed4b6d8751d61a4ee3ceddddca10b00
Lucinda Sisk <lucinda.sisk@gmail.com>
2019-10-08 12:01:15 -0400

    added eddy_squad script

---
+++ eddy_squad.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+#Usage: ./eddy_qc <name_of_subject_list.txt
+
+#SBATCH --job-name=shapes_eddycuda
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:2
+#SBATCH --mem-per-cpu=10G
+#SBATCH --time=3:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=lucinda.sisk@yale.edu
+#SBATCH --gres=gpu:1
+
+ml load StdEnv
+ml load FSL/6.0.1-centos7_64
+
+home='/gpfs/milgram/project/gee_dylan/candlab'
+quad=$home'/analyses/shapes/dwi/data/4_Eddy_QC_Data'
+cd $quad
+quad_fldrs=$(ls)
+
+eddy_squad $quad_fldrs
