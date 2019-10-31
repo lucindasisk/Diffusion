e8ccdf8a3d95503dd888419ba19bff2d8f49faba
Lucinda Sisk <lucinda.sisk@gmail.com>
2019-10-31 09:50:36 -0400

    update

---
+++ sbatch_3_DTI_Tractography_tensor.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=shapes_tractography_manualsubs
+#SBATCH --ntasks=1 --nodes=1
+#SBATCH --mem-per-cpu=10G
+#SBATCH --time=12:00:00
+#SBATCH --partition=long
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=lucinda.sisk@yale.edu
+
+ ml load StdEnv
+ ml load FreeSurfer/6.0.0
+ ml load FSL/6.0.1-centos7_64
+ ml load Python/miniconda
+ ml load ANTs/2.3.1-foss-2018a
+
+source activate /gpfs/milgram/project/gee_dylan/lms233/conda_envs/shapes_dwi
+
+home='/gpfs/milgram/project/gee_dylan/candlab/scripts/shapes/mri/dwi/Diffusion'
+python $home/3_Tractography_Pipeline_tensoronly.py
