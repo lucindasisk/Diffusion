a4d872d257260cb35a0150805e1d0da6c244ae13
Lucinda Sisk <lucinda.sisk@gmail.com>
2019-10-31 11:58:19 -0400

    change to GPU partition

--- sbatch_3_DTI_Tractography.sh
+++ sbatch_3_DTI_Tractography.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1 --nodes=1
 #SBATCH --mem-per-cpu=10G
 #SBATCH --time=12:00:00
-#SBATCH --partition=long
+#SBATCH --partition=gpu
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=lucinda.sisk@yale.edu
 
