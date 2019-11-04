324b4e5cb8c82da5435b117b1009e1fbc2ed50e4
Lucinda Sisk <lucinda.sisk@gmail.com>
2019-11-04 09:48:01 -0500

    fix issues

--- clean_workflow.sh
+++ clean_workflow.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
 home='/gpfs/milgram/project/gee_dylan/candlab/analyses/shapes/dwi/workflows/tract_flow/'
-
+cd $home
 subs=$(ls)
 
 for sub in $subs ; do
+  cd $home
   rm -R $sub'/trkconvert'
 done
