d23802b55fead17ec01cc38705b0552c0505edf8
Lucinda Sisk <lucinda.sisk@gmail.com>
2019-11-05 19:53:13 -0500

    add sbatch to fsr reconall

--- freesurfer_recon_all.sh
+++ freesurfer_recon_all.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+#SBATCH --job-name=freesurfer_reconall
+#SBATCH --ntasks=1 --nodes=1
+#SBATCH --mem-per-cpu=10G
+#SBATCH --time=12:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=lucinda.sisk@yale.edu
+
+ml load FreeSurfer/6.0.0
 export SUBJECTS_DIR='/gpfs/milgram/project/gee_dylan/candlab/data/mri/shapes_freesurfer'
 
 sub=$1
