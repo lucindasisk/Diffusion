5fa7cd964836b09131130c9444d88a6c0b9e4528
Lucinda Sisk <lucinda.sisk@gmail.com>
2019-11-06 10:24:11 -0500

    update

--- clean_workflow.sh
+++ clean_workflow.sh
@@ -3,8 +3,10 @@
 home='/gpfs/milgram/project/gee_dylan/candlab/analyses/shapes/dwi/workflows/tract_flow/'
 cd $home
 subs=$(ls)
+dir='mscsd'
 
 for sub in $subs ; do
+  echo 'removing '$dir' directory for '$sub
   cd $home
-  rm -R $sub'/mscsd'
+  rm -R $sub'/'$dir
 done
