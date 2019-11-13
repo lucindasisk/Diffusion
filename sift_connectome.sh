ff017616f7b57d2e92c5040e03b7f55a03104f79
Lucinda Sisk <lucinda.sisk@gmail.com>
2019-11-13 10:42:50 -0500

    update

--- sift_connectome.sh
+++ sift_connectome.sh
@@ -13,9 +13,9 @@ sub=$1
 
 home='/gpfs/milgram/project/gee_dylan/candlab'
 fbin='/gpfs/milgram/project/gee_dylan/lms233/fsr_bin'
-dwi=$home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/msCSD_brain_tracktography.tck'
-actfile=$home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/T1s_5tt_segmented.nii.gz'
-wmfod=$home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/wm.mif'
+dwi=$home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/msCSD_brain_tracktography.tck'
+actfile=$home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/T1s_5tt_segmented.nii.gz'
+wmfod=$home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/wm.mif'
 t1=$home'/data/mri/shapes_freesurfer/'$sub'/mri/brain.mgz'
 echo 'Starting '$sub'!'
 
@@ -29,45 +29,45 @@ fsraseg=$home'/data/mri/shapes_freesurfer/'$sub'/mri/aparc.a2009s+aseg.nii.gz'
 #Rename FreeSurfer LUT nodes
 labelconvert $home'/data/mri/shapes_freesurfer/'$sub'/mri/aparc.a2009s+aseg.nii.gz' \
 $fbin'/FreeSurferColorLUT.txt' $fbin'/fs_default.txt' \
-$home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/'$sub'_parcellation_LUT.mif'
+$home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/'$sub'_parcellation_LUT.mif'
 
 # Extract ROIs of interest
 # echo 'Extracting bilateral amygdala and frontal cortex for '$sub
-# fslmaths $fsraseg -thr 17.5 -uthr 18.5 -bin $home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/left_amyg.nii.gz'
-# fslmaths $fsraseg -thr 53.5 -uthr 54.5 -bin $home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/right_amyg.nii.gz'
-# fslmaths $fsraseg -thr 11100.5 -uthr 11101.5 -bin $home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/left_cortex.nii.gz'
-# fslmaths $fsraseg -thr 12100.5 -uthr 12101.5 -bin $home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/right_cortex.nii.gz'
-# fslmaths $fsraseg -thr 46.5 -uthr 48.5 -bin $home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/left_cerebellum.nii.gz'
-# fslmaths $fsraseg -thr 7.5 -uthr 8.5 -bin $home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/right_cerebellum.nii.gz'
+# fslmaths $fsraseg -thr 17.5 -uthr 18.5 -bin $home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/left_amyg.nii.gz'
+# fslmaths $fsraseg -thr 53.5 -uthr 54.5 -bin $home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/right_amyg.nii.gz'
+# fslmaths $fsraseg -thr 11100.5 -uthr 11101.5 -bin $home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/left_cortex.nii.gz'
+# fslmaths $fsraseg -thr 12100.5 -uthr 12101.5 -bin $home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/right_cortex.nii.gz'
+# fslmaths $fsraseg -thr 46.5 -uthr 48.5 -bin $home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/left_cerebellum.nii.gz'
+# fslmaths $fsraseg -thr 7.5 -uthr 8.5 -bin $home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/right_cerebellum.nii.gz'
 #
 #
 # # Combine binarized ROIs into one file for connectome analysis
 # echo 'Creating combined mask of all 4 ROIs for '$sub
-# fslmaths $home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/left_amyg.nii.gz' \
-#  -add $home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/right_amyg.nii.gz' \
-#  -add $home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/left_cortex.nii.gz' \
-#  -add $home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/right_cortex.nii.gz' \
-#  -add $home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/left_cerebellum.nii.gz' \
-#  -add $home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/right_cerebellum.nii.gz' \
-# $home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/combined_connectome_rois.nii.gz'
+# fslmaths $home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/left_amyg.nii.gz' \
+#  -add $home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/right_amyg.nii.gz' \
+#  -add $home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/left_cortex.nii.gz' \
+#  -add $home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/right_cortex.nii.gz' \
+#  -add $home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/left_cerebellum.nii.gz' \
+#  -add $home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/right_cerebellum.nii.gz' \
+# $home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/combined_connectome_rois.nii.gz'
 
 #Perform SIFT for tract data
-if [ -e $home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/SIFT_msCSD_brain_tracktography.tck' ] ; then
+if [ -e $home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/SIFT_msCSD_brain_tracktography.tck' ] ; then
   echo 'SIFT output already exists for '$subs
 else
   echo 'Running SIFT for '$sub
   tcksift -act $actfile $dwi $wmfod \
-  $home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/SIFT_msCSD_brain_tracktography.tck'
+  $home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/SIFT_msCSD_brain_tracktography.tck'
 fi
 
 echo 'Generating connectome for '$sub
 tck2connectome -force -zero_diagonal \
-$home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/SIFT_msCSD_brain_tracktography.tck' \
+$home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/SIFT_msCSD_brain_tracktography.tck' \
 $home'/data/mri/shapes_freesurfer/'$sub'/mri/aparc.a2009s+aseg.nii.gz' \
-$home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/'$sub'_wholeseg_SIFT_msCSD_connectome.csv'
+$home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/'$sub'_wholeseg_SIFT_msCSD_connectome.csv'
 
 echo 'Generating shortened connectome for '$sub
 tck2connectome -force -zero_diagonal \
-$home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/SIFT_msCSD_brain_tracktography.tck' \
-$home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/combined_connectome_rois.nii.gz' \
-$home'/analyses/shapes/dwi/data/5_tract_Reconstruction/'$sub'/'$sub'_ROI_SIFT_msCSD_connectome.csv'
+$home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/SIFT_msCSD_brain_tracktography.tck' \
+$home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/combined_connectome_rois.nii.gz' \
+$home'/analyses/shapes/dwi/data/5_Tract_Reconstruction/'$sub'/'$sub'_ROI_SIFT_msCSD_connectome.csv'
