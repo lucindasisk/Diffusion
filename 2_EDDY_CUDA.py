d01426693d696727bcf44bf0c231fa176489f363
Lucinda Sisk <lucinda.sisk@gmail.com>
2019-11-15 19:28:17 -0500

    update eddy sublist

--- 2_EDDY_CUDA.py
+++ 2_EDDY_CUDA.py
@@ -4,8 +4,8 @@
 # In[1]:
 
 
-from nipype.interfaces.io import DataSink, SelectFiles, DataGrabber
-from nipype.interfaces.utility import IdentityInterface, Function
+from nipype.interfaces.io import DataSink, SelectFiles, DataGrabber 
+from nipype.interfaces.utility import IdentityInterface, Function    
 from nipype.pipeline.engine import Node, Workflow, JoinNode, MapNode
 from nipype.interfaces import fsl
 import nipype.interfaces.freesurfer as fsr
@@ -22,33 +22,18 @@ from os.path import join, expanduser
 # Set variables
 user = expanduser('~')
 
-# Set user for laptop
-if user == '/Users/lucindasisk':
-    base = '/Users/lucindasisk/Desktop/Milgram/candlab'
-    home = join(base, 'analyses/shapes/dwi')
-    data_dir = join(base, 'analyses/shapes/dwi/eddyCUDA_data')
-    workflow_dir = join(base, 'analyses/shapes/dwi/eddyCUDA_workflow')
-    raw_dir = join(base, 'data/mri/bids_recon/shapes')
-
-# Set user for Grace
-if user == '/home/fas/gee_dylan/lms233':
-    home = getcwd() + '/..'
-    data_dir = join(home, 'eddyCUDA_data')
-    workflow_dir = join(home, 'eddyCUDA_workflow')
-
-# Set user for Milgram
+#Set user for Milgram
 if user == '/home/lms233':
     home = '/gpfs/milgram/project/gee_dylan/candlab'
-    data_dir = join(home, 'analyses/shapes/dwi/data')
+    data_dir = join(home,'analyses/shapes/dwi/data')
     workflow_dir = join(home, 'analyses/shapes/dwi/workflows')
     raw_dir = join(home, 'data/mri/bids_recon/shapes')
-
-# Manual subject list
-# subject_list = ['sub-A200', 'sub-A202'] #, 'sub-A687', 'sub-A694', 'sub-A695', 'sub-A698']  # , 'sub-A201']
-
+    
+## Manual subject list
+#subject_list = ['sub-A200', 'sub-A202'] #, 'sub-A687', 'sub-A694', 'sub-A695', 'sub-A698']  # , 'sub-A201']
+    
 # Read in subject subject_list
-subject_csv = read_csv(
-    home + '/scripts/shapes/mri/dwi/shapes_dwi_subjList_11.15.2019.txt', sep=' ', header=None)
+subject_csv = read_csv(home + '/scripts/shapes/mri/dwi/shapes_dwi_subjList_11.15.2019.txt', sep=' ', header=None)
 subject_list = subject_csv[0].values.tolist()
 
 
@@ -100,7 +85,7 @@ fslroi = Node(fsl.ExtractROI(t_min=0,
 stripb0 = Node(fsl.BET(mask=True, output_type='NIFTI_GZ'),
                name='stripb0')
 
-# Eddy_CUDA Node
+#Eddy_CUDA Node
 # FSL Eddy correction to remove eddy current distortion
 
 eddy = Node(fsl.Eddy(is_shelled=True,
@@ -114,11 +99,11 @@ eddy = Node(fsl.Eddy(is_shelled=True,
                      args="--ol_type='both' --ol_pos='true'"),
             name='eddy')
 
-# Resample dti to isotropic 1.7x1.7x1.7
+#Resample dti to isotropic 1.7x1.7x1.7
 resample = Node(fsr.Resample(voxel_size=(1.7, 1.7, 1.7)),
-                name="resample")
+               name = "resample")
 
-# Take out these nodes per this "best practices" discussion: https://community.mrtrix.org/t/diffusion-in-mni-space/367
+## Take out these nodes per this "best practices" discussion: https://community.mrtrix.org/t/diffusion-in-mni-space/367
 
 # #Reorient to standard
 # reorient = Node(fsl.Reorient2Std(output_type='NIFTI_GZ'),
@@ -139,24 +124,22 @@ resample = Node(fsr.Resample(voxel_size=(1.7, 1.7, 1.7)),
 
 eddy_flow = Workflow(name='eddy_flow')
 eddy_flow.connect([(infosource, sf, [('subject_id', 'subject_id')]),
-                   # Resample DTI to uniform dimensions
+                   #Resample DTI to uniform dimensions
                    (sf, fslroi, [('dti', 'in_file')]),
-                   # Skullstrip b0 volume
+                   #Skullstrip b0 volume
                    (fslroi, stripb0, [('roi_file', 'in_file')]),
-                   # Save b0 mask
-                   (stripb0, datasink, [
-                    ('mask_file', '3_Eddy_Corrected.@par')]),
-                   # Run Eddy correction
+                   #Save b0 mask
+                   (stripb0, datasink, [('mask_file', '3_Eddy_Corrected.@par')]),
+                   #Run Eddy correction
                    (sf, eddy, [('dti', 'in_file'),
                                ('bval', 'in_bval'),
                                ('bvec', 'in_bvec'),
                                ('index', 'in_index'),
                                ('aps', 'in_acqp')]),
                    (stripb0, eddy, [('mask_file', 'in_mask')]),
-                   # Save Eddy outputs
+                   #Save Eddy outputs
                    (eddy, datasink, [('out_corrected', '3_Eddy_Corrected.@par.@par'),
-                                     ('out_rotated_bvecs',
-                                      '3_Eddy_Corrected.@par.@par.@par'),
+                                     ('out_rotated_bvecs', '3_Eddy_Corrected.@par.@par.@par'),
                                      ('out_movement_rms',
                                       '3_Eddy_Corrected.@par.@par.@par.@par'),
                                      ('out_parameter',
@@ -170,10 +153,9 @@ eddy_flow.connect([(infosource, sf, [('subject_id', 'subject_id')]),
                                      ('out_residuals',
                                       '3_Eddy_Corrected.@par.@par.@par.@par.@par.@par.@par.@par.@par'),
                                      ('out_outlier_report',
-                                      '3_Eddy_Corrected.@par.@par.@par.@par.@par.@par.@par.@par.@par.@par')]),
+                                      '3_Eddy_Corrected.@par.@par.@par.@par.@par.@par.@par.@par.@par.@par')]),   
                    (eddy, resample, [('out_corrected', 'in_file')]),
-                   (resample, datasink, [
-                    ('resampled_file', '3_Eddy_Corrected.@par.@par.@par.@par.@par.@par.@par.@par.@par.@par.@par')])
+                   (resample, datasink, [('resampled_file', '3_Eddy_Corrected.@par.@par.@par.@par.@par.@par.@par.@par.@par.@par.@par')])      
                    ])
 eddy_flow.base_dir = workflow_dir
 eddy_flow.write_graph(graph2use='flat')
@@ -183,9 +165,9 @@ eddy = eddy_flow.run('MultiProc', plugin_args={'n_procs': 4})
 # In[ ]:
 
 
-# eddyqc nodes
+#eddyqc nodes
 
-# eddyquad = Node(fsl.EddyQuad(),
+# eddyquad = Node(fsl.EddyQuad(),     
 #                name='eddyquad')
 
 
@@ -209,3 +191,7 @@ eddy = eddy_flow.run('MultiProc', plugin_args={'n_procs': 4})
 
 
 # In[ ]:
+
+
+
+
