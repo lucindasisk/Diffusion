665a465ac7064f920e2c467a7f30afc30f6caf21
Lucinda Sisk <lucinda.sisk@gmail.com>
2019-11-19 18:49:26 -0500

    rerun seg5tt

--- clean_workflow.sh
+++ clean_workflow.sh
@@ -3,7 +3,7 @@
 home='/gpfs/milgram/project/gee_dylan/candlab/analyses/shapes/dwi/workflows/tract_flow/'
 cd $home
 subs=$(ls)
-dir='mscsd'
+dir='seg5tt'
 
 for sub in $subs ; do
   echo 'removing '$dir' directory for '$sub
