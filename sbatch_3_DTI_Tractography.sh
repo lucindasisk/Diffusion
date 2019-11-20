9ad265ef501aa4667bc97f7e94afcefad31a805f
Lucinda Sisk <lucinda.sisk@gmail.com>
2019-11-20 10:10:18 -0500

    change time

--- sbatch_3_DTI_Tractography.sh
+++ sbatch_3_DTI_Tractography.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=10G
 #SBATCH --time=80:00:00
-#SBATCH --partition=long
+#SBATCH --partition=verylong
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=lucinda.sisk@yale.edu
 
