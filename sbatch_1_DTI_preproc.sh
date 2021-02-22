370d15b7d0aa86d3a91ca17ae05ba9aec5e0d37b
Lucinda Sisk <lucinda.sisk@gmail.com>
2021-02-22 15:01:59 -0700

    update

--- sbatch_1_DTI_preproc.sh
+++ sbatch_1_DTI_preproc.sh
@@ -11,14 +11,13 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=lucinda.sisk@yale.edu
 
+source activate /gpfs/milgram/project/gee_dylan/lms233/conda_envs/shapes_dwi
+
  ml load StdEnv
  ml load FreeSurfer/6.0.0
  ml load FSL/6.0.1-centos7_64
  ml load MRtrix3/3.0_RC3-foss-2018a
  ml load Python/miniconda
- ml load ANTs/2.3.1-foss-2018a
-
-source activate /gpfs/milgram/project/gee_dylan/lms233/conda_envs/shapes_dwi
 
 home='/gpfs/milgram/project/gee_dylan/candlab/scripts/shapes/mri/dwi/Diffusion'
 python $home/1_DTI_Preprocessing-SHAPES.py
