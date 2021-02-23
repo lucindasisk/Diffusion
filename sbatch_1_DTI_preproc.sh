54cf00a64f6b52f66a86b61cc661f2fb3032c01c
Lucinda Sisk <lucinda.sisk@gmail.com>
2021-02-22 18:53:25 -0700

    add second gpu

--- sbatch_1_DTI_preproc.sh
+++ sbatch_1_DTI_preproc.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --mem-per-cpu=10G
 #SBATCH --time=12:00:00
 #SBATCH --mail-type=ALL
