116a8f166138640f149c7005299d68d2bc5fb47f
Lucinda Sisk <lucinda.sisk@gmail.com>
2021-02-22 18:56:27 -0700

    add second gpu

--- sbatch_1_DTI_preproc.sh
+++ sbatch_1_DTI_preproc.sh
@@ -17,7 +17,7 @@
  ml load MRtrix3/3.0_RC3-foss-2018a
  ml load Python/3.6.4-foss-2018a
  ml load ANTs/2.3.1-foss-2018a
- ml load CUDA/7.5.18
+ ml load CUDA/9.1.85-GCC-6.4.0-2.28
 
 source activate /gpfs/milgram/project/gee_dylan/lms233/conda_envs/shapes-dwi3.7
 
