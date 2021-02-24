ef550de3fe6fc32c0814aaea3a986d77e16f3170
Lucinda Sisk <lucinda.sisk@gmail.com>
2021-02-24 09:07:58 -0700

    update cuda information

--- sbatch_1_DTI_preproc.sh
+++ sbatch_1_DTI_preproc.sh
@@ -13,11 +13,11 @@
 
  ml load StdEnv
  ml load FreeSurfer/6.0.0
- ml load FSL/6.0.0
+ ml load FSL/6.0.3-centos7_64
  ml load MRtrix3/3.0_RC3-foss-2018a
  ml load Python/3.6.4-foss-2018a
  ml load ANTs/2.3.1-foss-2018a
- ml load CUDA/7.5.18
+ ml load CUDA/10.2.89
 
 source activate /gpfs/milgram/project/gee_dylan/lms233/conda_envs/shapes-dwi3.7
 
