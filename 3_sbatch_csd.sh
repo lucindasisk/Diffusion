8397d9b7592c30df87b70fd7d2d6ec7361c711a7
Lucinda Sisk <lucinda.sisk@gmail.com>
2021-03-11 18:24:34 -0500

    debug

---
+++ 3_sbatch_csd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name=shapes_preprocessing
+#SBATCH --ntasks=1 --nodes=1
+#SBATCH --mem-per-cpu=10G
+#SBATCH --time=12:00:00
+#SBATCH --partition=long
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=lucinda.sisk@yale.edu
+source ~/.bashrc
+
+ml load StdEnv
+ml load Python/3.6.4-foss-2018a
+
+conda activate /gpfs/milgram/pi/gee_dylan/lms233/conda_envs/shapes-dwi3.7
+
+home='/home/lms233/Github/Diffusion'
+python $home/3_Deconvolution_Pipeline.py $1
---
+++ dsq-3_csd_dsq_job_array-2021-03-11.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --output dsq-3_csd_dsq_job_array-%A_%3a-%N.out
+#SBATCH --array 0-140
+#SBATCH --job-name dsq-3_csd_dsq_job_array
+#SBATCH --mem-per-cpu 20g -t 10:00:00 --mail-type ALL
+
+# DO NOT EDIT LINE BELOW
+/gpfs/milgram/apps/hpc.rhel7/software/dSQ/1.05/dSQBatch.py --job-file /gpfs/milgram/home/lms233/Github/Diffusion/3_csd_dsq_job_array.txt --status-dir /gpfs/milgram/home/lms233/Github/Diffusion
+
